ae23378e0c679779c9be903ddee40acd2ca6ef55
Gabriel Gomes <100102446+gabrxgomes@users.noreply.github.com>
2023-05-09 15:49:33 -0300

    Adicionando Teste de tipos Primitivos em java

---
+++ testeTiposPrimitivos/TesteTiposPrimitivos/src/testeTiposPrimitivosPrincipal/classeTeste.java
@@ -0,0 +1,11 @@
+package testeTiposPrimitivosPrincipal;
+
+public class classeTeste {
+	public char x;
+	
+	public char printarChar(){
+		System.out.println(x);
+		
+		return x;
+	}
+}
---
+++ testeTiposPrimitivos/TesteTiposPrimitivos/src/testeTiposPrimitivosPrincipal/principal.java
@@ -0,0 +1,18 @@
+package testeTiposPrimitivosPrincipal;
+
+import testeTiposPrimitivosPrincipal.classeTeste;
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		classeTeste classeTesteobj;
+		
+		classeTesteobj = new classeTeste();
+		
+		
+		System.out.println();
+
+	}
+
+}
